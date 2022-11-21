85ef4321b5c8c620e51978f5495ae6061e338768
Sarlyt <sara.castro@alumno.buap.mx>
2022-11-21 14:49:24 -0600

    Quite weon y agrege teibol

--- pages/user.js
+++ pages/user.js
@@ -11,7 +11,8 @@ import {
   Label,
   Input,
   FormText,
-} from 'reactstrap';
+  Container,
+} from "reactstrap";
 
 export default function Home() {
   return (
@@ -24,47 +25,45 @@ export default function Home() {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>
-        Hola weon este es tu perfil
-      </h1>
-      <Row className="mt-3">
-              <Col md= "6" xs="12">
-                <div className="bg-light p-2 border">.col-6</div>
-                <Form>
-                  <FormGroup>
-                    <Label for="exampleUser">Usuario</Label>
-                    <Input
-                      required 
-                      id="exampleUser"
-                      name="usuario"
-                      placeholder="Usuario"
-                    />
-                  </FormGroup>
-                  <FormGroup>
-                    <Label for="exampleEmail">Correo</Label>
-                    <Input
-                      id="exampleEmail"
-                      name="email"
-                      placeholder="Correo"
-                      type="email"
-                    />
-                  </FormGroup>
-                  <FormGroup>
-                    <Label for="examplePassword">Contraseña</Label>
-                    <Input
-                      id="examplePassword"
-                      name="password"
-                      placeholder="Tu Contraseña"
-                      type="password"
-                    />
-                  </FormGroup>
-                  <Button>Enviar</Button>
-                </Form>
-              </Col>
-              <Col xs="6">
-                <div className="bg-light p-2 border">.col-6</div>
-              </Col>
-            </Row>
+      <h1>Hola weon este es tu perfil</h1>
+      <Container>
+        <Row className="mt-3">
+          <Col md="6" xs="12">
+            <Form>
+              <FormGroup>
+                <Label for="exampleUser">Usuario</Label>
+                <Input
+                  required
+                  id="exampleUser"
+                  name="usuario"
+                  placeholder="Usuario"
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label for="exampleEmail">Correo</Label>
+                <Input
+                  id="exampleEmail"
+                  name="email"
+                  placeholder="Correo"
+                  type="email"
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label for="examplePassword">Contraseña</Label>
+                <Input
+                  id="examplePassword"
+                  name="password"
+                  placeholder="Tu Contraseña"
+                  type="password"
+                />
+              </FormGroup>
+              <Button>Enviar</Button>
+            </Form>
+          </Col>
+          <Col xs="6">
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
--- pages/zitas.js
+++ pages/zitas.js
@@ -1,4 +1,19 @@
 import Head from "next/head";
+import {
+    Table,
+    Card,
+    Container,
+    Row,
+    Col,
+    CardTitle,
+    CardBody,
+    Button,
+    Form,
+    FormGroup,
+    Label,
+    Input,
+    FormText,
+} from 'reactstrap';
 
 export default function Home() {
     return (
@@ -12,6 +27,42 @@ export default function Home() {
             <link rel="icon" href="/favicon.ico" />
         </Head>
         <h1>Hola weon, aqui estan tus consultas</h1>
+        <Container>
+            <Row>
+                <Table hover>
+                    <thead>
+                        <tr>
+                        <th>#</th>
+                        <th>Status</th>
+                        <th>Identificador</th>
+                        <th>Médico</th>
+                        <th>Fecha</th>
+                        <th>Hora</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>@fat</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td>Larry</td>
+                            <td>the Bird</td>
+                            <td>@twitter</td>
+                        </tr>
+                    </tbody>
+                </Table>
+            </Row>
+        </Container>
     </div>
 );
 }
