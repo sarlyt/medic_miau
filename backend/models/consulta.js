d2e069e17663b4b0917e2410d481ad2867281044
Antonio Islas <antonio.islas@smartkode.mx>
2022-11-26 14:14:57 -0600

    siuu

---
+++ backend/models/consulta.js
@@ -0,0 +1,17 @@
+
+module.exports = (sequelize, type)=>{
+    return sequelize.define('consulta',{
+        id:{
+            type: type.INTEGER,
+            primaryKey: true,
+            autoIncrement:true
+        },
+        status:type.STRING, 
+        id_medico:type.INTEGER,
+        id_usuario:type.INTEGER,
+        descripcion:type.STRING,
+    });
+}
+
+
+//status espera|agendada|cancelada|finalizada
